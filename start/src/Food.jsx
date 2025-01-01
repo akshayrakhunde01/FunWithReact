8e0cabe384268fc59335ff175f569ff1430bdb13
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-02 00:25:48 +0530

    fixed bug

--- start/src/Food.jsx
+++ start/src/Food.jsx
@@ -2,13 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Food(){
     let foodItem=['dal','khichadi','shevbhaji','onion','more']
-
+    // console.log(foodItem.length)
     return(
         <>
         <h1>Welcome to food items</h1>
         <ul className='list-group'>
-           { foodItem.map((item)=>{
-            return <li className='list-group-item'>{item}</li>
+           { foodItem.map((item,index)=>{
+            return <li className='list-group-item' key={item}>{index}</li>
            })}
         </ul>
         </>
